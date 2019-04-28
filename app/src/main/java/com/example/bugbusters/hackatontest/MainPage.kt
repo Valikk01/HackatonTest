885c07ab6222afd6fc8c8bb6142d8d1c41a89208
Valikk01 <datsko1212@gmail.com>
2019-04-28 21:13:38 +0300

    New Event

--- app/src/main/java/com/example/bugbusters/hackatontest/MainPage.kt
+++ app/src/main/java/com/example/bugbusters/hackatontest/MainPage.kt
@@ -1,22 +1,41 @@
 package com.example.bugbusters.hackatontest
-
+import android.app.FragmentTransaction
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_page.*
 
-class MainPage : AppCompatActivity() {
+
+class MainPage : AppCompatActivity(),EventFragment.OnFragmentInteractionListener {
+
+    lateinit var newEvent : EventFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        newEvent = EventFragment.newInstance()
+
+        fab.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame,newEvent)
+                .addToBackStack(newEvent.toString())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
         }
+
+
+//
     }
 
+    fun change (){
+        setContentView(R.layout.activity_main_page)
+    }
+
+
+    override fun onFragmentInteraction(uri: Uri?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
